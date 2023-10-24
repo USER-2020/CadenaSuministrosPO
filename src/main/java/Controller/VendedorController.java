216942a16493d60788ce3a50bae603a70a47f7fd
USER-2020 <hackingetico2021@gmail.com>
2023-10-24 15:59:19 -0500

    Changues in controller, add ViewGUIUSers

--- src/main/java/Controller/VendedorController.java
+++ src/main/java/Controller/VendedorController.java
@@ -5,6 +5,8 @@
 package Controller;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import model.Usuario;
 import model.Vendedor;
 
 /**
@@ -16,36 +18,80 @@ public class VendedorController implements ICRUD{
 
     @Override
     public boolean insertar(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        boolean insertar = true;
+        Vendedor seller = new Vendedor();
+        seller = (Vendedor) obj;
+        if(arregloVendedores.isEmpty()){
+            arregloVendedores.add(seller);
+        }else{
+            arregloVendedores.add(seller);
+        }
+        return insertar;
     }
 
     @Override
-    public Object consultar(int idProducto) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public Object consultar(int idVendedor) {
+        for(Vendedor seller: arregloVendedores){
+            if(seller.getId() == idVendedor){
+                return seller;
+            }            
+        }
+        return null; 
     }
 
     @Override
     public void consultar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        for(Vendedor seller: arregloVendedores){
+            System.out.println("Vendedores" + seller.toString());
+        }
     }
 
     @Override
-    public Object actualizar(int idProducto) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public Object actualizar(int idVendedor) {
+        for (Vendedor vendedor : arregloVendedores) {
+        if (vendedor.getId() == idVendedor) {                       
+            // Realiza la lógica de actualización aquí, por ejemplo, cambiar propiedades del vendedor.
+            // Después de actualizar, puedes devolver el objeto actualizado o un indicador de éxito.
+            vendedor.setNombre(vendedor.getNombre());
+            vendedor.setApellido(vendedor.getApellido());
+            vendedor.setTipoUSer(Usuario.TipoUsuario.Vendedor);
+            vendedor.setDireccion(vendedor.getDireccion());
+            vendedor.setEmail(vendedor.getEmail());
+            vendedor.setCellphone(vendedor.getCellphone());
+            
+            return vendedor; // Devuelve el objeto actualizado
+        }
+    }
+    // Si el vendedor con el ID dado no se encontró, puedes devolver null o lanzar una excepción, según tu preferencia.
+    return null;
     }
 
     @Override
     public boolean borrar(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    if (obj instanceof Vendedor) {
+        Vendedor vendedorABorrar = (Vendedor) obj;
+        boolean eliminado = arregloVendedores.remove(vendedorABorrar);
+        return eliminado;
+    }
+    return false;
     }
 
     @Override
     public boolean ordenar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        if(!arregloVendedores.isEmpty()){
+            Collections.sort(arregloVendedores);
+            return true;
+        }
+        return false;
     }
 
     @Override
     public Object consultar(Object obj) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+         Vendedor seller = (Vendedor) obj;
+        int pos = Collections.binarySearch(arregloVendedores, seller);
+        if(pos > 0 )
+            return arregloVendedores.get(pos);
+        else
+            return null;
     }
 }
